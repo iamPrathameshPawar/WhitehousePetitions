21001f919013e24b415d7cf3505cf53669a3b448
prathamesh.pawar <prathamesh.pawar@blancco.com>
2025-03-22 13:04:08 +0530

    Initial Commit

---
+++ WhitehousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Prathamesh Pawar on 3/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
